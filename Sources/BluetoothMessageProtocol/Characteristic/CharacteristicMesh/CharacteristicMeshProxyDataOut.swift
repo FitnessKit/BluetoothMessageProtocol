f9502597c1cee2a31d9adaa5d67e370a0a7a6fee
Kevin A. Hoogheem <kevinh@npe-inc.com>
2018-07-04 10:17:21 -0500

    mesh

---
+++ Sources/BluetoothMessageProtocol/Characteristic/CharacteristicMesh/CharacteristicMeshProxyDataOut.swift
@@ -0,0 +1,8 @@
+//
+//  CharacteristicMeshProxyDataOut.swift
+//  BluetoothMessageProtocol
+//
+//  Created by Kevin Hoogheem on 7/4/18.
+//
+
+import Foundation
---
+++ Sources/BluetoothMessageProtocol/Types/ProvisioningDataUnitType.swift
@@ -0,0 +1,8 @@
+//
+//  ProvisioningDataUnitType.swift
+//  BluetoothMessageProtocol
+//
+//  Created by Kevin Hoogheem on 7/4/18.
+//
+
+import Foundation
