e0d0dec017771c2ba67295641acde78792e5b833
Kevin A. Hoogheem <kevinh@npe-inc.com>
2018-07-04 14:00:19 -0500

    mesh cleanup

---
+++ Sources/BluetoothMessageProtocol/Mesh/Provisioning/ProvisioningDataUnitCapabilities.swift
@@ -0,0 +1,184 @@
+//
+//  ProvisioningDataUnitCapabilities.swift
+//  BluetoothMessageProtocol
+//
+//  Created by Kevin Hoogheem on 7/4/18.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Foundation
+
+/// Provisioning Data Unit Capabilities
+///
+/// The device sends this PDU to indicate its supported provisioning capabilities to a Provisioner
+public struct ProvisioningDataUnitCapabilities: ProvisioningDataUnit {
+
+    /// Provisioning Protocol Data Unit Type
+    private(set) public var unitType: ProvisioningDataUnitType
+
+    /// Algorithms
+    public struct AlgorithmType: OptionSet {
+        public let rawValue: UInt16
+        public init(rawValue: UInt16) { self.rawValue = rawValue }
+
+        /// FIPS P-256 Elliptic Curve Supported
+        public static let fipsP256  = AlgorithmType(rawValue: 1 << 0)
+    }
+
+    /// Public Key Type
+    public struct PublicKeyType: OptionSet {
+        public let rawValue: UInt8
+        public init(rawValue: UInt8) { self.rawValue = rawValue }
+
+        /// Public Key OOB information available
+        public static let publicKeyOobAvailable     = PublicKeyType(rawValue: 1 << 0)
+    }
+
+    /// Public Key Type
+    public struct StaticOobType: OptionSet {
+        public let rawValue: UInt8
+        public init(rawValue: UInt8) { self.rawValue = rawValue }
+
+        /// Static OOB information available
+        public static let staticOobAvailable    = StaticOobType(rawValue: 1 << 0)
+    }
+
+    /// Supported Output OOB Actions
+    public struct OutputActions: OptionSet {
+        public let rawValue: UInt16
+        public init(rawValue: UInt16) { self.rawValue = rawValue }
+
+        /// Blink Supported
+        public static let blink         = OutputActions(rawValue: 1 << 0)
+        /// Beep Supported
+        public static let beep          = OutputActions(rawValue: 1 << 1)
+        /// Virbrate Supported
+        public static let vibrate       = OutputActions(rawValue: 1 << 2)
+        /// Output Numeric Supported
+        public static let numeric       = OutputActions(rawValue: 1 << 3)
+        /// Output Alphanumeric Supported
+        public static let alphanumeric  = OutputActions(rawValue: 1 << 4)
+    }
+
+    /// Supported Input OOB Actions
+    public struct InputActions: OptionSet {
+        public let rawValue: UInt16
+        public init(rawValue: UInt16) { self.rawValue = rawValue }
+
+        /// Push Supported
+        public static let push          = InputActions(rawValue: 1 << 0)
+        /// Twist Supported
+        public static let twist         = InputActions(rawValue: 1 << 1)
+        /// Input Numeric Supported
+        public static let numeric       = InputActions(rawValue: 1 << 2)
+        /// Output Alphanumeric Supported
+        public static let alphanumeric  = InputActions(rawValue: 1 << 3)
+    }
+
+    /// Number of Elements
+    ///
+    /// Number of elements supported by the device
+    private(set) public var elements: ProvisioningSize
+
+    /// Algorithms
+    ///
+    /// Supported algorithms and other capabilities
+    private(set) public var algorithm: AlgorithmType
+
+    /// Public Key
+    ///
+    /// Supported public key types
+    private(set) public var publicKey: PublicKeyType
+
+    /// Static OOB Type
+    ///
+    /// Supported static OOB Types
+    private(set) public var staticOobTypes: StaticOobType
+
+    /// Output OOB Size
+    ///
+    /// Maximum size of Output OOB supported
+    private(set) public var outputObbSize: ProvisioningSize
+
+    /// Output OOB Action
+    ///
+    /// Supported Output OOB Actions
+    private(set) public var supportedOutputActions: OutputActions
+
+    /// Input OOB Size
+    ///
+    /// Maximum size in octets of Input OOB supported
+    private(set) public var inputOobSize: ProvisioningSize
+
+    /// Input OOB Action
+    ///
+    /// Supported Input OOB Actions
+    private(set) public var supportedInputActions: InputActions
+
+    /// Create Provisioning Data Unit
+    ///
+    /// - Parameters:
+    ///   - elements: Number of elements supported by the device
+    ///   - algorithm: Supported algorithms and other capabilities
+    ///   - publicKey: Supported public key types
+    ///   - staticOobTypes: Supported static OOB Types
+    ///   - outputObbSize: Maximum size of Output OOB supported
+    ///   - supportedOutputActions: Supported Output OOB Actions
+    ///   - inputOobSize: Maximum size in octets of Input OOB supported
+    ///   - supportedInputActions: Supported Input OOB Actions
+    public init(elements: ProvisioningSize,
+                algorithm: AlgorithmType,
+                publicKey: PublicKeyType,
+                staticOobTypes: StaticOobType,
+                outputObbSize: ProvisioningSize,
+                supportedOutputActions: OutputActions,
+                inputOobSize: ProvisioningSize,
+                supportedInputActions: InputActions) {
+
+        self.unitType = .capabilities
+        self.elements = elements
+        self.algorithm = algorithm
+        self.publicKey = publicKey
+        self.staticOobTypes = staticOobTypes
+        self.outputObbSize = outputObbSize
+        self.supportedOutputActions = supportedOutputActions
+        self.inputOobSize = inputOobSize
+        self.supportedInputActions = supportedInputActions
+    }
+
+    /// Encodes Provisioning Protocol Data Unit into Data
+    ///
+    /// - Returns: Encoded Data
+    /// - Throws: BluetoothMessageProtocolError
+    public func encode() throws -> Data {
+        var msgData = Data()
+
+        msgData.append(unitType.rawValue)
+        msgData.append(elements.rawValue)
+        msgData.append(Data(from: algorithm.rawValue.littleEndian))
+        msgData.append(publicKey.rawValue)
+        msgData.append(staticOobTypes.rawValue)
+        msgData.append(outputObbSize.rawValue)
+        msgData.append(Data(from: supportedOutputActions.rawValue.littleEndian))
+        msgData.append(inputOobSize.rawValue)
+        msgData.append(Data(from: supportedInputActions.rawValue.littleEndian))
+
+        return msgData
+    }
+}
